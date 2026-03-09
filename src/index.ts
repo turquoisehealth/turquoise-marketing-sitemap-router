1ceb2182d7c1e53759a94377e8f9518dc55e3166
Andrew Bloom <andrew@turquoise.health>
2026-03-09 15:29:25 -0500

    add tokens

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,40 @@
+const sitemapUrl = "https://marketing.turquoise.health/sitemap.xml";
+
 export default {
-	async fetch(request: Request): Promise<Response> {
-		const remote = "https://marketing.turquoise.health/sitemap.xml";
-		return await fetch(remote, request);
+	async fetch(request) {
+		function MethodNotAllowed(request) {
+			return new Response(`Method ${request.method} not allowed.`, {
+				status: 405,
+				headers: {
+					Allow: "GET",
+				},
+			});
+		}
+		// Only GET requests work with this proxy.
+		if (request.method !== "GET") return MethodNotAllowed(request);
+
+		// Best practice is to always use the original request to construct the new request
+		// to clone all the attributes. Applying the URL also requires a constructor
+		// since once a Request has been constructed, its URL is immutable.
+		const newRequest = new Request(sitemapUrl, request);
+
+		newRequest.headers.set("cf-access-client-id", env.CF_ACCESS_CLIENT_ID);
+		newRequest.headers.set("cf-access-client-secret", env.CF_ACCESS_CLIENT_SECRET);
+
+		try {
+			const response = await fetch(newRequest);
+
+			// Copy over the response
+			const modifiedResponse = new Response(response.body, response);
+
+			// Delete the set-cookie from the response so it doesn't override existing cookies
+			modifiedResponse.headers.delete("set-cookie");
+
+			return modifiedResponse;
+		} catch (e) {
+			return new Response(JSON.stringify({ error: e.message }), {
+				status: 500,
+			});
+		}
 	},
 };
